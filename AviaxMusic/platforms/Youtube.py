edaf67c94656df4d0594427671290eb798232e99
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2025-07-27 18:45:08 +0530

    Update Youtube.py

--- AviaxMusic/platforms/Youtube.py
+++ AviaxMusic/platforms/Youtube.py
@@ -8,8 +8,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from BRANDEDKING.utils.database import is_on_off
-from BRANDEDKING.utils.formatters import time_to_seconds
+from AviaxMusic.utils.database import is_on_off
+from AviaxMusic.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
