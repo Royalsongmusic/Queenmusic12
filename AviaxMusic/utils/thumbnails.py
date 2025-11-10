532bbb9b9dd2596604df8e41a41d9746fa9d3f80
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2025-11-10 16:54:47 +0530

    Update thumbnails.py

--- AviaxMusic/utils/thumbnails.py
+++ AviaxMusic/utils/thumbnails.py
@@ -4,7 +4,7 @@ import aiofiles
 import aiohttp
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont
 from youtubesearchpython.__future__ import VideosSearch
-from config import YOUTUBE_IMG_URL
+from py_yt import VideosSearch
 
 # Constants
 CACHE_DIR = "cache"
